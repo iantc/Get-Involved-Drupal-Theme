a3c16c90b30549423320f337ee865d44e5867d50
Ian C <ian@ik2.us>
2013-03-25 23:00:55 -0700

    improve search result entries

--- templates/search-result.tpl.php
+++ templates/search-result.tpl.php
@@ -1,40 +1,33 @@
-<?php print $search_totals; ?>
 <li class="search-result">
-  <h3 class="title"><a href="<?php print $url; ?>"><?php print $title; ?></a></h3>
+  <h3 class="title"><a href="<?php print $url; ?>"><?php print $title; ?></a>
+
+  </h3>
   <?php if ($snippet or $info_split): ?>
     <div class="search-snippet-info">
       <?php if ($snippet) : ?>
-        <p class="search-snippet"><?php print $snippet; ?></p>
+        <div class="search-snippet"><?php print $snippet; ?></div>
       <?php endif; ?>
 
       <?php if ($info_split) : ?>
-        <p class="search-info">
-          <?php $separator = ''; ?>
-
-          <?php if (isset($info_split['type'])) : ?>
-            <span class="search-info-type"><?php print $info_split['type']; ?></span>
-            <?php $separator = $info_separator; ?>
-          <?php endif; ?>
-
-          <?php if (isset($info_split['user'])) : ?>
-            <span class="search-info-user"><?php print $separator . $info_split['user']; ?></span>
-            <?php $separator = $info_separator; ?>
-          <?php endif; ?>
-
+        <div class="search-info">
           <?php if (isset($info_split['date'])) : ?>
-            <span class="search-info-date"><?php print $separator . $info_split['date']; ?></span>
+            <span class="search-info-date">
+              <?php print format_date($result['node']->changed,"custom","M j, Y"); ?>
+            </span>
             <?php $separator = $info_separator; ?>
           <?php endif; ?>
+          <span class="search-info-type"><?php print $info_split['type']; ?></span>
+          <div class="pull-right">
+            <?php if (isset($info_split['comment'])) : ?>
+              <span class="search-info-comment"><?php print $separator . $info_split['comment']; ?></span>
+              <?php $separator = $info_separator; ?>
+            <?php endif; ?>
 
-          <?php if (isset($info_split['comment'])) : ?>
-            <span class="search-info-comment"><?php print $separator . $info_split['comment']; ?></span>
-            <?php $separator = $info_separator; ?>
-          <?php endif; ?>
-
-          <?php if (isset($info_split['upload'])) : ?>
-            <span class="search-info-upload"><?php print $separator . $info_split['upload']; ?></span>
-          <?php endif; ?>
-        </p>
+            <?php if (isset($info_split['upload'])) : ?>
+              <span class="search-info-upload"><?php print $separator . $info_split['upload']; ?></span>
+            <?php endif; ?>
+          </div>
+        </div>
       <?php endif; ?>
     </div>
   <?php endif; ?>
