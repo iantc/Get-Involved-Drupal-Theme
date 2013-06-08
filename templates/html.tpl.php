5c5dcd820ef4d31276576700379c4564b460e1fd
Ian C <ian.t.c1995@gmail.com>
2013-06-08 14:09:54 -0700

    Add viewport to head of template

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,21 +5,29 @@
 <!--[if (IE 8)&(!IEMobile)]><html class="ie8 ie6-8" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes . $rdf_namespaces; ?>><!--<![endif]-->
 <head>
-<?php print $head; ?>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="MobileOptimized" content="width">
-<meta name="HandheldFriendly" content="true">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta http-equiv="cleartype" content="on">
-<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-<title><?php print $head_title; ?></title>
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
-<link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css" rel="stylesheet">
-<?php print $styles; ?>
-<?php print $scripts; ?>
-<!--[if lt IE 9]>
-<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
+  <?php print $head; ?>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="MobileOptimized" content="width">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="apple-mobile-web-app-capable" content="yes">
+  <meta http-equiv="cleartype" content="on">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
+  <title><?php print $head_title; ?></title>
+  <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.min.css" rel="stylesheet">
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+  <script type="text/javascript">
+    jQuery(document).ready(function(){
+      jQuery('#menu-bar .menu').addClass('nav nav-tabs');
+      jQuery('#menu-bar .menu a.active').parent().addClass('active');
+      jQuery('#search-form').next('ul').wrap('<div class="alert alert-info" id="search-help">');
+      jQuery('#search-help').prepend('<h3>Need Help?</h3>');
+    });
+  </script>
+  <!--[if lt IE 9]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <div id="skip-link">
